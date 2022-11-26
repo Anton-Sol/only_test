a9b2dcf47887344eaf1c24e581b3160d7f295c34
Nightingale <solwork550@gmail.com>
2022-11-26 12:45:39 +0300

    hello

--- new_task1.py
+++ new_task1.py
@@ -1,4 +1,6 @@
-
+'''
+asdasfagfwdafs
+'''
 from functools import reduce
 print(sum(i for i in range(1,1000) if i%3 == 0 or i%5 == 0 ))
 print(sum([i for i in range(1,1000) if i%3 == 0 or i%5 == 0] ))
